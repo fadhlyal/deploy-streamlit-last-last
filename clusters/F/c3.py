16d449c66737cf3c97b1dda8a33da14bb7de39e6
Fadhly Al-farizi <74035663+fadhlyal@users.noreply.github.com>
2025-01-12 22:52:54 +0000

    dataF2

--- clusters/F/c3.py
+++ clusters/F/c3.py
@@ -1,2 +1,10 @@
-def problemF(N) :
-    return N
+def problemF(N):
+    result = ""
+    
+    if(N < 0) :
+        result = "negative"
+    elif(N > 0) :
+        result = "zero"
+    else :
+        result = "positive"
+    return result
